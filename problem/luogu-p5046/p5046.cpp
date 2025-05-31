3d51e4aca64e3ac69af32721a4f76936db917cdc
xu-haozhe <xu_haozhe@icloud.com>
2025-06-01 00:04:55 +0800

    20250601 00:04 保存p5046(尚未完成)

--- problem/luogu-p5046/p5046.cpp
+++ problem/luogu-p5046/p5046.cpp
@@ -10,39 +10,61 @@ template<class T>inline void read_(T&t_)noexcept{
 __always_inline void read()noexcept{}
 template<class T,class ...Args>__always_inline void read(T&first,Args&...args)noexcept{read_(first);read(args...);}
 using ll=long long;using ull=unsigned long long;
-constexpr uint maxn=1e5,maxblocknum=320;
-template<class T,T*P>struct iterid{
-    constexpr static T*p=P;uint id;
-    constexpr iterid(uint id):id(id){}
-    inline operator uint()const{return id;}
-    __always_inline T& operator*()const{return p[id];}
-    __always_inline T* operator->()const{return p+id;}
-};
-namespace fenwick{
-    alignas(4096)uint val[1<<17];
-    inline void add(uint p,uint v)noexcept{for(;p<(1<<!7);p+=p&-p)val[p]+=v;}
-    inline uint query(uint p)noexcept{ull res=0;for(;p;p&=p-1)res+=val[p];return res;}
-    __always_inline void clear()noexcept{bzero(val,sizeof(val));}
-}
+using blockid=uint;using arrid=uint;
+constexpr ull hightbit(ull x)noexcept{return x?(1ull<<(63-__builtin_clzll(x))):(0);}
+constexpr uint maxn=1e5,maxblocknum=320,maxblocksize=320;
+constexpr uint maxn_pow2=hightbit(maxn)<<1,maxblocksize_pow2=hightbit(maxblocksize)<<1;
+int n,m,blocknum,blocksize;
+alignas(4096)uint arr[maxn+10],pre[maxn+10],suf[maxn+10],sum[maxblocknum][1<<!7];
+ull f[maxblocknum+10][maxblocknum+10];
+struct fenwick{
+    alignas(4096) static uint val[maxn_pow2];
+    static inline void add(uint p,uint v)noexcept{for(;p<(1<<17);p+=p&-p)val[p]+=v;}
+    static inline uint query(uint p)noexcept{ull res=0;for(;p;p&=p-1)res+=val[p];return res;}
+}fenwick;
+alignas(4096) uint fenwick::val[maxn_pow2];
 struct SortArr{
     uint v,p;
     __always_inline bool operator<(SortArr&b)const noexcept{return v<b.v;}
-}sort_arr[maxn+10];
+};alignas(maxblocksize_pow2<<1)SortArr sort_arr[maxblocknum+10][maxblocksize_pow2];
 struct block{
     uint id,l,r;
-}blocks[maxblocknum+10];
-using blockid=iterid<block,blocks>;
-inline uint get_merge_ans(blockid a,blockid b,uint b1,uint e1,uint b2,uint e2)noexcept{
-    auto*i1=sort_arr+a->l,*i2=sort_arr+b->l,*end1=sort_arr+a->r,*end2=sort_arr+b->r;
-    ull res=0,cnt=0;
-    while(true){
-        while(i1!=end1&(i1->p<b1|i1->p>=e1))++i1;
-        while(i2!=end2&(i2->p<b2|i2->p>=e2))++i2;
+}blocks[maxblocknum+10],*e_block;blockid p_block[maxn+10];
+struct merge{
+    alignas(512) static uint a[maxblocksize_pow2],b[maxblocksize_pow2];
+    static inline uint calc()noexcept{
+        uint res=0,*ia=a,*ib=b;
+        while(*ia&*ib)if(*ia<*ib)++ia,res+=ib-b;else ++ib;
+        return res;
     }
-    return res;
-}
+    static inline  uint calc(blockid a,blockid b)noexcept{
+        uint res;
+        for(auto*i=sort_arr[a],*j=sort_arr[b],*b=j;*(ull*)i&*(ull*)j;)
+            if(i->v<j->v)++i,res+=j-b;else ++j;
+        return res;
+    }
+}merge_ans;
+
 int main(){
-    int a,b;ull c;
-    read(a,b,c);
+    read(n,m),blocksize=sqrt(n),blocknum=(n+blocksize-1)/blocksize,e_block=blocks+blocknum;
+    for(int i=0;i<n;i++)read(arr[i]);
+    for(uint i=0,cnt=0;cnt<n;i++,cnt+=blocksize)blocks[i]={i,cnt,cnt+blocksize};
+    blocks[blocknum-1].r=n,blocks[blocknum]={(uint)blocknum,(uint)n,(uint)n};
+    for(auto*i=blocks;i!=e_block;i++){
+        for(uint j=i->l;j<i->r;j++)sort_arr[i->id][j-i->l]={arr[j],j},p_block[j]=i->id;
+        sort(sort_arr[i->id],sort_arr[i->id]+i->r-i->l,[](SortArr const&a,SortArr const&b){return a.v<b.v;});
+        for(uint j=i->l;j!=i->r;j++){
+            pre[j]=fenwick.query(0x1ffff-arr[j]);
+            fenwick.add(0x1ffff-arr[j],1);
+        }
+        f[i->id][i->id+1]=pre[i->r-1];
+        for(uint*j=arr+i->l,*e=arr+i->r;j!=e;j++)fenwick.add(0x1ffff-*j,-1);
+        for(uint j=i->r-1;j>=i->l;j--){
+            suf[j]=fenwick.query(arr[j]);
+            fenwick.add(arr[j],1);
+        }
+        for(uint*j=arr+i->l,*e=arr+i->r;j!=e;j++)fenwick.add(*j,-1);
+    }
+    for(int len=1;len<=n;len++)for(int i=0;i<=n-len;i++)f[i][i+len]=f[i][i+len-1]+f[i+1][i+len]-merge_ans.calc(i,i+len-1)-f[i+1][i+len-1];
     return 0;
 }
