f64d1bf8fe89a84204f7e9f6aa2332829531e3fa
xu-haozhe <150572790+xu-haozhe@users.noreply.github.com>
2025-06-10 22:41:19 +0800

    20250610 22:24  保存

--- problem/luogu-p6772/p6772.cpp
+++ problem/luogu-p6772/p6772.cpp
@@ -2,8 +2,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll=long long;using ull=unsigned long long;
+template<class T>inline void read(T&t_)noexcept{
+    unsigned char ch=getchar();T t=0;
+    while(!isdigit(ch))ch=getchar();
+    while(isdigit(ch))t=t*10+ch-'0',ch=getchar();
+    t_=t;[[assume(t_>=0)]];
+}
+template<class T,class ...Args>__always_inline void read(T&first,Args&...args)noexcept{read_(first);read(args...);}
 constexpr ll MIN=-1e16;
-struct alignas(8<<16) matrix256x256{ll a[256][256];}a;
+struct alignas(8<<16) matrix256x256{ll a[256][256];}a,b;
 struct alignas(8<<8) vec256{ll a[256];};
 inline void mul(matrix256x256 &a,matrix256x256 &b,matrix256x256 &c){
     for(int i=0;i<256;i++){
@@ -15,10 +22,41 @@ inline void mul(matrix256x256 &a,matrix256x256 &b,matrix256x256 &c){
 }
 inline void mul(vec256 &a,matrix256x256 &b,vec256 &c){
     for(int i=0;i<256;i++){
-        for(int j=0;j<256;j++)
+        ll tmp=a.a[i],(&arr)[256]=b.a[i],(&arr2)[256]=c.a;
+        for(int j=0;j<256;j++)arr2[j]=max(arr2[j],arr[j]+tmp);
     }
 }
+template<class T>struct matrix_{T d[2],*a=d,*b=d+1;inline void swap()noexcept{swap(a,b);};};
+inline void pow(matrix256x256 const &a,ll n,matrix_<matrix256x256> &c){
+    matrix_<matrix256x256> tmp;
+    memcpy(tmp.a,&a,sizeof(matrix256x256));
+    {
+        auto&t=c.a->a;
+        for(auto&i:t)for(auto &j:i)j=MIN;
+        for(int i=0;i<256;i++)t[i][i]=0;
+    }
+    for(;n;n>>=1){
+        if(n&1ll)mul(*c.a,*tmp.a,*c.b),c.swap();
+        mul(*tmp.a,*tmp.a,*tmp.b),tmp.swap();
+    }
+}
+int n,m,k,c[55];ll T,t,y,lstt;
+matrix256x256 arr1;matrix_<matrix256x256>arr2;matrix_<vec256>vec;
 int main(){
-    
+    read(n,m,T,k);
+    for(int i=0;i<n;i++)read(c[i]);
+    for(auto&i:arr1.a)for(auto&j:i)j=MIN;
+    for(int u,v,w;m--;)read(u,v,w),arr1.a[u+w-2][v-1]=c[v-1];
+    for(auto&i:vec.a->a)i=MIN;vec.a->a[0]=c[0];
+    for(int x;k--;){
+        read(t,x,y);
+        pow(arr1,t-lstt,arr2);
+        mul(*vec.a,*arr2.a,*vec.b),vec.swap();
+        if(vec.a->a[x-1]>0)vec.a->a[x-1]+=y;
+        lstt=t;
+    }
+    pow(arr1,T-lstt,arr2);
+    mul(*vec.a,*arr2.a,*vec.b),vec.swap();
+    cout<<(vec.a->a[0])<<'\n';
     return 0;
 }
