79fa390fcca1f7ef07c6c922dcd017f26578b6fa
xu-haozhe <150572790+xu-haozhe@users.noreply.github.com>
2025-06-19 22:28:43 +0800

    save

--- problem/luogu-p7737/p7737.cpp
+++ problem/luogu-p7737/p7737.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+template<class T>inline void read(T&t){
+    unsigned char ch=getchar();T t=0;
+    while(!isdigit(ch))ch=getchar();
+    while(isdigit(ch))t=t*10+(ch&0xf),ch=getchar();
+    t_=t;[[assume(t_>=0)]];
+}
+template<class T,class...Args>__always_inline void read(T&first,Args&...args){read(first);read(args...);}
+using ll=long long;using ull=unsigned long long;
+constexpr size_t maxn=3e5;
+int n,m,q,k;
+namespace init_graph{
+    vector<uint>edges[maxn+10];
+}
 int main(){
 
     return 0;
