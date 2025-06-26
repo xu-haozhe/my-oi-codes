dc175bef66c4f9a8fd673fb1309006ce72418ad5
xu-haozhe <150572790+xu-haozhe@users.noreply.github.com>
2025-06-27 00:19:21 +0800

    p3376 AC

--- problem/luogu-p3376/p3376.cpp
+++ problem/luogu-p3376/p3376.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll=long long;
+int n,m,s,t,dep[205];
+ll ans;
+struct edge{int t,w;union Rev{int p;vector<edge>::iterator it;}rev;};
+vector<edge>edges[205];
+vector<edge>::iterator it[205];
 template<class T>inline void read(T&t_)noexcept{
     char ch=getchar();T t=0;char f=1;
     while(!isdigit(ch))ch=getchar();
@@ -8,46 +13,50 @@ template<class T>inline void read(T&t_)noexcept{
     t_=t*f;
 }
 template<class T,class ...Args>__always_inline void read(T&first,Args&...args)noexcept{read(first);read(args...);}
-int n,m,s,t,dep[205],cnt=1,h[205],now[205];ll ans;
-struct edge{int v,w,nxt;}edges[10005];
-inline void add_edge(int u,int v,int w)noexcept{edges[++cnt]=(edge){v,w,h[u]},h[u]=cnt;}
-inline bool bfs()noexcept{ 
-    bzero(dep+1,n*sizeof(*dep));
-    queue<int>q;q.push(s),dep[s]=1,now[s]=h[s];
+inline void add_edge(int u,int v,int w)noexcept{//u --w--> v
+    edges[u].push_back({v,w,{(int)edges[v].size()}});
+    edges[v].push_back({u,0,{(int)edges[u].size()-1}});
+}
+
+
+inline bool bfs()noexcept{
+    bzero(dep,(n+1)*sizeof(*dep));
+    queue<int>q;q.push(s),dep[s]=1,it[s]=edges[s].begin();
     while(!q.empty()){
         int u=q.front();q.pop();
-        for(int i=h[u];i;i=edges[i].nxt){
-            int v=edges[i].v,w=edges[i].w;
-            if(w&&!dep[v]){
-                dep[v]=dep[u]+1,now[v]=h[v];
-                q.push(v);
-                if(v==t)return 1;
-            }
+        for(auto [v,w,_]:edges[u])if((w>0)&(!dep[v])){
+           it[v]=edges[v].begin(),q.push(v),dep[v]=dep[u]+1;
+           if(v==t)return 1;
         }
     }
-    return 0;
+    return false;
 }
-ll dfs(int u,ll sum){
+ll dfs(int u,ll sum)noexcept{
     if(u==t)return sum;
-    ll k,res=0;
-    for(auto &i=now[u];
-        i&&sum;
-        i=edges[i].nxt){
-        // now[u]=i;
-        int v=edges[i].v,w=edges[i].w;
-        if(w&&(dep[v]==dep[u]+1)){
+    int d=dep[u]+1;ll k=0,res=0;
+    for(auto i=it[u];i->t&&sum;i++){
+        it[u]=i;
+        auto [v,w,rev]=*i;
+        if((w>0)&(dep[v]==d)){
             k=dfs(v,min(sum,(ll)w));
-            if(!k)dep[v]=0;
-            sum-=k,res+=k,edges[i].w-=k,edges[i^1].w+=k;
+            if(k==0)dep[v]=0;
+            i->w-=k,rev.it->w+=k,res+=k,sum-=k;
         }
     }
     return res;
 }
+
 int main(){
     read(n,m,s,t);
-    for(int i=0,u,v,w;i<m;i++)read(u,v,w),add_edge(u,v,w),add_edge(v,u,0);
-    while(bfs())
-        ans+=dfs(s,0xffffffffffff);
+    for(int i=1;i<=n;i++)edges[i].reserve(m+2);
+    for(int i=0,u,v,w;i<m;i++)read(u,v,w),add_edge(u,v,w);
+    for(int i=1;i<=n;i++){
+        edges[i].push_back({0});
+        for(auto &[v,w,rev]:edges[i])rev.it=edges[v].begin()+rev.p;
+    }
+    while(bfs()){
+        ans+=dfs(s,0xffffffffffffll);
+    }
     printf("%lld\n",ans);
     return 0;
 }
