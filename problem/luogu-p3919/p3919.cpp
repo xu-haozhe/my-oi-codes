3b15c41e111873e2110814f2fad2c2fc1fc37be4
xu-haozhe <150572790+xu-haozhe@users.noreply.github.com>
2025-06-29 03:08:09 +0800

    p3919 AC
    在线算法
    p3919.cpp 1.09s/92.99mb https://www.luogu.com.cn/record/221715131
    p3919.c 932ms/93.11mb https://www.luogu.com.cn/record/221715178
    
    使用了 https://www.luogu.com.cn/user/929850 大佬提交记录 https://www.luogu.com.cn/record/210503174 中的的io优化代码，c版本得以卡进 1s ，感谢大佬的代码

--- problem/luogu-p3919/p3919.cpp
+++ problem/luogu-p3919/p3919.cpp
@@ -1,38 +1,36 @@
+/*
+1.09s/92.99mb https://www.luogu.com.cn/record/221715131
+*/
 #include<bits/stdc++.h>
 using namespace std;
-
 template<class T>inline void read(T&t_)noexcept{
-    char ch=getchar();T t=0;char f=1;
-    while(!isdigit(ch))ch=getchar();
-    while(isdigit(ch))t=t*10+(ch&0xf),ch=getchar();
-    t_=t*f;
+    unsigned char ch=getchar_unlocked();T t=0;bool f=false;
+    while(!isdigit(ch)){if(ch=='-'){f=true;}ch=getchar_unlocked();}
+    while(isdigit(ch))t=t*10+(ch&0xf),ch=getchar_unlocked();
+    t_=f?-t:t;
 }
 template<class T,class ...Args>__always_inline void read(T&first,Args&...args)noexcept{read(first);read(args...);}
-
-using T=int;
-
-struct node{
-    T d;uint mask;uint p[20]; 
-}nodes[305];
-
-inline T find(uint v,uint i)noexcept{
-    while(nodes[v].mask^i)v=nodes[v].p[__builtin_ctz(nodes[v].mask^i)];
-    return nodes[v].d;
+int n,m,v,opt,p,c,arr[1000005],d[10000005];
+uint mask[10000005],ptr[1000005][20];
+inline int find(uint v,uint i)noexcept{
+    while((mask[v]^i)&&(v))v=ptr[v][__builtin_ctz(mask[v]^i)];
+    return v?d[v]:arr[i];
 }
-inline void add(uint v1,uint v2,uint p,T d)noexcept{ 
-    nodes[v2].d=d;
-    for(uint i=0,k=p^nodes[v1].mask;i<20;i++)nodes[v2].p[i]=d&(1u<<i)?v1=nodes[v1].p[i]:v1;
+inline void add(uint v1,uint v2,uint p,int d)noexcept{ 
+    ::d[v2]=d,mask[v2]=p;
+    auto&v2p=ptr[v2];
+    for(uint i=0;i<20;i++)
+        if((p^mask[v1])&(1u<<i))v2p[i]=v1,v1=ptr[v1][i];
+        else v2p[i]=ptr[v1][i];
 }
-inline void cp(uint v1,uint v2)noexcept{memcpy(nodes+v2,nodes+v1,sizeof(node));}
-
-int n,m,v,opt,p,c;
+inline void cp(uint v1,uint v2)noexcept{memcpy(ptr[v2],ptr[v1],20*sizeof(uint)),mask[v2]=mask[v1],d[v2]=d[v1];}
 int main(){
     read(n,m);
-    for(int i=1,val;i<=n;i++)read(val),add(i-1,i,i,val);
+    for(int i=1;i<=n;i++)read(arr[i]);
     for(int i=1;i<=m;i++){
         read(v,opt,p);
-        if(opt==1)read(c),add(v+n,i+n,p,c);
-        else cout<<find(v+n,p)<<'\n',cp(v+n,v+i);
+        if(opt==1)read(c),add(v,i,p,c);
+        else cout<<find(v,p)<<'\n',cp(v,i);
     }
     return 0;
 }
